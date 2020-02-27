4704f9f455ddd03fb20fc8f0940944dd5ffc18ec
Teisi <hackl.chris@googlemail.com>
2020-02-27 16:11:55 +0100

    bugfix typo

--- Configuration/TCA/Overrides/page.php
+++ Configuration/TCA/Overrides/page.php
@@ -3,12 +3,12 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(function() {
 
-    $extensionKey = "hh_gmaps_simmplest";
+    $extensionKey = 'hh_gmaps_simplest';
 
     // make PageTsConfig selectable
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
         $extensionKey,
-        "Configuration/TsConfig/AllPage.typoscript",
-        "EXT:{$extensionKey} :: Gmaps Simplest"
+        'Configuration/TsConfig/AllPage.typoscript',
+        'EXT:'.$extensionKey.' :: Gmaps Simplest'
     );
 });
